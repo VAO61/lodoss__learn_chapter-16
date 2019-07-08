a4c770d64c5f399a289fd6c59eada7aee7ba6675
VAO61 <vao.frontend@gmail.com>
2019-07-08 17:18:06 +0300

    Added markup & styles --pre-alpha

--- src/js/index.js
+++ src/js/index.js
@@ -4,5 +4,6 @@ if (process.env.NODE_ENV !== 'production') {
   require('file-loader!../html/index.html');
 }
 
-import '../../node_modules/rxjs/bundles/rxjs.umd.min.js';
-import './app/readmeRx.js';
+// import '../../node_modules/rxjs/bundles/rxjs.umd.min.js';
+// import './about/readmeRx.js';
+// import './about/readmeRx_fromEvent';
